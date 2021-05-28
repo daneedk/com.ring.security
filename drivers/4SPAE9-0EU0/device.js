93c15c58f2e1140450a2c7f4354330b90627b376
Danee de Kruijff <daneedekruyff@outlook.com>
2021-05-28 21:25:59 +0200

    Motion Detector

---
+++ drivers/4SPAE9-0EU0/device.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { ZwaveDevice } = require('homey-zwavedriver');
+
+class RingDevice extends ZwaveDevice {
+
+  async onNodeInit() {
+    // this.enableDebug();
+    // this.printNode();
+
+    // register Homey's capabilities with Z-Wave COMMAND CLASSES
+    this.registerCapability('measure_battery', 'BATTERY');
+    
+    // register listener for NOTIFICATION REPORT
+    this.registerReportListener('NOTIFICATION', 'NOTIFICATION_REPORT', report =>  {
+      //this.log(report);
+      switch (report['Notification Type']) {
+        case "Home Security":
+          if ( report['Event'] == 8 ) {
+            this.setCapabilityValue('alarm_motion', true)
+          } else if ( report['Event'] == 0 ) {
+            this.setCapabilityValue('alarm_motion', false)
+          }
+          break;
+      }
+    });
+  }
+}
+
+module.exports = RingDevice;
