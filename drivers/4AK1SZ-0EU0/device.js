ab3c89377007fb17090035b416cc916a58920a71
Danee de Kruijff <daneedekruyff@outlook.com>
2022-08-29 13:08:09 +0200

    Added Siren to Keypad V2

--- drivers/4AK1SZ-0EU0/device.js
+++ drivers/4AK1SZ-0EU0/device.js
@@ -11,15 +11,29 @@ const INDICATOR_ID_OK = 0x08;
 const INDICATOR_ID_NOT_OK = 0x09;
 const INDICATOR_ID_ARMED_HOME = 0x0A;
 const INDICATOR_ID_ARMED_AWAY = 0x0B;
+const INDICATOR_ID_ALARM_GENERIC = 0x0C;
+const INDICATOR_ID_ALARM_BURGLAR = 0x0D;
+const INDICATOR_ID_ALARM_SMOKE = 0x0E;
+const INDICATOR_ID_ALARM_CARBON = 0x0F;
 const INDICATOR_ID_BYPASS = 0x10;
 const INDICATOR_ID_ENTRY_DELAY = 0x11;
 const INDICATOR_ID_EXIT_DELAY = 0x12;
+const INDICATOR_ID_ALARM_MEDICAL = 0x13;
+const INDICATOR_ID_CHIME_DOUBLEBEEP = 0x60;
+const INDICATOR_ID_CHIME_GUITARRIFF = 0x61;
+const INDICATOR_ID_CHIME_WINDCHIMES = 0x62;
+const INDICATOR_ID_CHIME_BINGBONG = 0x63;
+const INDICATOR_ID_CHIME_RINGDOORBELL = 0x64;
+
 //Property IDs
 const PROPERTY_ID_MULTILEVEL = 0x01;
 const PROPERTY_ID_BINARY = 0x02;
+const PROPERTY_ID_DURATION = 0x07;
+const PROPERTY_ID_VOLUME = 0x09;
 // Indicator Values
 const INDICATOR_VALUE_OFF = 0x00;
 const INDICATOR_VALUE_ON = 0xFF;
+const INDICATOR_VOLUME_HALF = 0x32;
 // Indicator commandos
 const INDICATOR_DISARMED = { id: INDICATOR_ID_NOT_ARMED,property: PROPERTY_ID_BINARY,value: INDICATOR_VALUE_ON, }
 const INDICATOR_OK = { id: INDICATOR_ID_OK,property: PROPERTY_ID_BINARY,value: INDICATOR_VALUE_ON, }
@@ -29,14 +43,24 @@ const INDICATOR_ARMED_AWAY = { id: INDICATOR_ID_ARMED_AWAY,property: PROPERTY_ID
 const INDICATOR_ARMED_BYPASS = { id: INDICATOR_ID_BYPASS,property: PROPERTY_ID_BINARY,value: INDICATOR_VALUE_ON, }
 const INDICATOR_ENTRY_DELAY = { id: INDICATOR_ID_ENTRY_DELAY,property: PROPERTY_ID_BINARY,value: INDICATOR_VALUE_ON, }
 const INDICATOR_EXIT_DELAY = { id: INDICATOR_ID_EXIT_DELAY,property: PROPERTY_ID_BINARY,value: INDICATOR_VALUE_ON, }
+const INDICATOR_ALARM_GENERIC = { id: INDICATOR_ID_ALARM_GENERIC,property: PROPERTY_ID_BINARY,value: INDICATOR_VALUE_ON, }
+const INDICATOR_ALARM_BURGLAR = { id: INDICATOR_ID_ALARM_BURGLAR,property: PROPERTY_ID_BINARY,value: INDICATOR_VALUE_ON, }
+const INDICATOR_ALARM_SMOKE = { id: INDICATOR_ID_ALARM_SMOKE,property: PROPERTY_ID_BINARY,value: INDICATOR_VALUE_ON, }
+const INDICATOR_ALARM_CARBON = { id: INDICATOR_ID_ALARM_CARBON,property: PROPERTY_ID_BINARY,value: INDICATOR_VALUE_ON, }
+const INDICATOR_ALARM_MEDICAL = { id: INDICATOR_ID_ALARM_MEDICAL,property: PROPERTY_ID_BINARY,value: INDICATOR_VALUE_ON, }
+const INDICATOR_CHIME_DOUBLEBEEP = { id: INDICATOR_ID_CHIME_DOUBLEBEEP,property: PROPERTY_ID_VOLUME,value: INDICATOR_VOLUME_HALF, }
+const INDICATOR_CHIME_GUITARRIFF = { id: INDICATOR_ID_CHIME_GUITARRIFF,property: PROPERTY_ID_VOLUME,value: INDICATOR_VOLUME_HALF, }
+const INDICATOR_CHIME_WINDCHIMES = { id: INDICATOR_ID_CHIME_WINDCHIMES,property: PROPERTY_ID_VOLUME,value: INDICATOR_VOLUME_HALF, }
+const INDICATOR_CHIME_BINGBONG = { id: INDICATOR_ID_CHIME_BINGBONG,property: PROPERTY_ID_VOLUME,value: INDICATOR_VOLUME_HALF, }
+const INDICATOR_CHIME_RINGDOORBELL = { id: INDICATOR_ID_CHIME_RINGDOORBELL,property: PROPERTY_ID_VOLUME,value: INDICATOR_VOLUME_HALF, }
 
 class RingDevice extends ZwaveDevice {
 
   async onNodeInit() {
     // this.enableDebug();
     // this.printNode();
-    // const commandClassIndicator = this.getCommandClass('INDICATOR');
-    // this.log("Indicator",commandClassIndicator);
+
+    this.ringOnce = false;
 
     // register the measure_battery capability with COMMAND_CLASS_BATTERY
     this.registerCapability('measure_battery', 'BATTERY');
@@ -48,11 +72,43 @@ class RingDevice extends ZwaveDevice {
     // Conditions
     // Actions
 
+    /*
+    this.homey.flow.getActionCard('4AK1SZ-0EU0-activateSiren')
+      .registerRunListener( async ( args, state ) => {
+          this.activateSiren(args.sirenMode);
+          return Promise.resolve( true );
+      })
+    this.homey.flow.getActionCard('4AK1SZ-0EU0-deactivateSiren')
+      .registerRunListener( async ( args, state ) => {
+          this.deactivateSiren();
+          return Promise.resolve( true );
+      })  
+    this.homey.flow.getActionCard('4AK1SZ-0EU0-soundChime')
+      .registerRunListener( async (args, state) => {
+        this.soundChime(args.chime,args.volume);
+        return Promise.resolve( true );
+      });   
+    */  
+
     // register listener for Heimdall events
     this.homey.app.heimdallApp
       .on('realtime', (result,detail) => {
           this.updateKeypadFromHeimdall(result,detail);
       })
+
+    // register listener for Ring events (Work in Progress)
+    /*
+    this.homey.app.ringApp
+    .on('realtime', (result,detail) => {
+      if ( !this.ringOnce ) {
+        this.updateKeypadFromRing(result,detail);
+        if ( detail === "ding" ) {
+          this.ringOnce = true;
+          setTimeout( () => {this.ringOnce = false}, 250);
+        }
+      }
+    })
+    */
 
     // register listener for NOTIFICATION REPORT
     this.registerReportListener('NOTIFICATION', 'NOTIFICATION_REPORT', report =>  {
@@ -85,34 +141,6 @@ class RingDevice extends ZwaveDevice {
       } else {
         this.codeString = "";
       }
-
-      // TESTCODE TESTCODE TESTCODE TESTCODE TESTCODE 
-      // TESTCODE TESTCODE TESTCODE TESTCODE TESTCODE 
-      // TESTCODE TESTCODE TESTCODE TESTCODE TESTCODE 
-      /*
-      if ( report['Event Type'] == "CANCEL" ) {
-        console.log("Command CANCEL entered");
-        //this.setIndicator(INDICATOR_DISARMED);
-        this.node.sendCommand(
-          this.IndicatorSet([INDICATOR_DISARMED])
-        );
-      }
-      
-      if ( report['Event Type'] == "ENTER" ) {
-        console.log("Command ENTER entered");
-        this.valueToSend = { id: this.codeString,property: PROPERTY_ID_BINARY,value: 0x00, }
-        console.log("value", this.valueToSend)
-        //this.setIndicator(this.valueToSend);
-        this.node.sendCommand(
-          this.IndicatorSet([this.valueToSend,])
-        );
-        return;
-      }
-      */
-      // TESTCODE TESTCODE TESTCODE TESTCODE TESTCODE 
-      // TESTCODE TESTCODE TESTCODE TESTCODE TESTCODE 
-      // TESTCODE TESTCODE TESTCODE TESTCODE TESTCODE 
-
       if ( report['Event Type'] == "POLICE" || report['Event Type'] == "FIRE" || report['Event Type'] == "ALERT_MEDICAL" ) {
         // Trigger flowcard that sends the emergency key
         var tokens = { actionkey: report['Event Type']};
@@ -207,6 +235,12 @@ class RingDevice extends ZwaveDevice {
 
       case "Arming Delay left": 
         if ( !this.activeArmingDelayStart ) {
+          let INDICATOR_EXIT_DELAY_TIMED = { id: INDICATOR_ID_EXIT_DELAY,property: PROPERTY_ID_DURATION,value: detail, }
+          this.setIndicator(INDICATOR_EXIT_DELAY_TIMED);
+          this.activeArmingDelayStart = true;
+        }
+        /*
+        if ( !this.activeArmingDelayStart ) {
           this.setIndicator(INDICATOR_EXIT_DELAY);
           this.activeArmingDelayStart = true;
           this.armingCounter = 1;
@@ -225,23 +259,36 @@ class RingDevice extends ZwaveDevice {
 
         }
         this.armingCounter += 1;
-
+        */  
         break;
 
       case "Alarm Delay left":
-        // Work in progress
-        // Work in progress
-        // Work in progress
+        if ( !this.activeAlarmDelayStart ) {
+          let INDICATOR_ENTRY_DELAY_TIMED = { id: INDICATOR_ID_ENTRY_DELAY,property: PROPERTY_ID_DURATION,value: detail, }
+          this.setIndicator(INDICATOR_ENTRY_DELAY_TIMED);
+          this.activeAlarmDelayStart = true;
+        }
         /*
-        if ( !this.activeArmingDelayStart ) {
+        if ( !this.activeAlarmDelayStart ) {
           this.setIndicator(INDICATOR_ENTRY_DELAY);
           this.activeAlarmDelayStart = true;
-          this.activeAlarmDelay = false;
           this.alarmCounter = 1;
+          if ( detail > 67 ) {
+            this.activeAlarmDelay = false;
+          } else {
+            this.activeAlarmDelay = true;
+          }
+
+        } else if ( detail > 67 && this.alarmCounter/10 == Math.floor(this.alarmCounter/10) && !this.activeAlarmDelay ) {
+          this.setIndicator(INDICATOR_ENTRY_DELAY);
+
+        } else if ( detail < 60 && !this.activeAlarmDelay ) {
+          this.setIndicator(INDICATOR_ENTRY_DELAY);
+          this.activeAlarmDelay = true;
 
         }
+        this.alarmCounter += 1;
         */
-
         break;
 
       case "Sensor State at Arming":
@@ -263,12 +310,27 @@ class RingDevice extends ZwaveDevice {
         this.log("Received an Alarm State of:", detail)
         if ( detail ) {
           if ( this.getSetting('usesiren') != "0" ) { 
-            // Work in progress
-            // Work in progress
-            // Work in progress
-            // Here be sirene
-            // this.setIndicator(xxSIRENExx);
+            switch (this.getSetting('usesiren')) {
+              case "BURGLAR":
+                this.setIndicator(INDICATOR_ALARM_BURGLAR);
+                break;
+              case "SMOKE":
+                this.setIndicator(INDICATOR_ALARM_SMOKE);
+                break;
+              case "MEDICAL":
+                this.setIndicator(INDICATOR_ALARM_MEDICAL);
+                break;
+            }
             this.log("ALARM!!!")  // Indiana Jones Style!
+          }
+        } else {
+          switch ( this.homey.app.heimdall.surveillancemode ) {
+            case "partially_armed":
+              this.setIndicator(INDICATOR_ARMED_HOME);
+              break;
+            case "armed":
+              this.setIndicator(INDICATOR_ARMED_AWAY);
+              break;
           }
         }
         
@@ -287,6 +349,86 @@ class RingDevice extends ZwaveDevice {
     }
   }
   
+  // called from the Ring event listener
+  /*
+  async updateKeypadFromRing(result,detail) {
+    // this.log(result, detail);
+    if ( result === "doorbell" ) {
+      if ( detail === "ding" ) {
+        this.log("Ring Video Doorbell ding event received:",this.getSetting('usechime'));
+        if ( this.getSetting('usechime') != "0") {
+          // TODO:
+          // Read volume and construct indicator value
+          let CHIME_ID = getSetting('usechime')
+          let VOLUME = getSetting('chimeVolume') * 20 - 1
+          let INDICATOR_CHIME = { id: CHIME_ID,property: PROPERTY_ID_VOLUME,value: VOLUME, }
+          this.setIndicator(INDICATOR_CHIME);
+        }
+      } else if ( detail === "motion" ) {
+        this.log("Ring Video Doorbell motion event received");
+      }
+    }
+  }
+  */
+
+  // Flowcard actions
+  /*
+  async activateSiren(sirenMode) {
+    this.log("Received activate Siren from flowcard with sirenmode:", sirenMode)
+    switch (sirenMode.toUpperCase()) {
+      case "BURGLAR":
+        this.setIndicator(INDICATOR_ALARM_BURGLAR);
+        break;
+      case "SMOKE":
+        this.setIndicator(INDICATOR_ALARM_SMOKE);
+        break;
+      case "MEDICAL":
+        this.setIndicator(INDICATOR_ALARM_MEDICAL);
+        break;
+    }
+  }
+
+  async deactivateSiren() {
+    switch ( await this.homey.app.heimdall.surveillancemode ) {
+      case "partially_armed":
+        this.setIndicator(INDICATOR_ARMED_HOME);
+        break;
+      case "armed":
+        this.setIndicator(INDICATOR_ARMED_AWAY);
+        break;
+      default:
+        this.setIndicator(INDICATOR_DISARMED);
+        break;
+    }    
+  }
+  */
+  /*
+  async soundChime(chime, volume) {
+    this.log("Received play Chime from flowcard with chime:", chime, volume)
+    let CHIME_ID = INDICATOR_ID_CHIME_RINGDOORBELL
+    switch (chime) {
+      case "doublebeep":
+        CHIME_ID = INDICATOR_ID_CHIME_DOUBLEBEEP
+        break;
+      case "guitarriff":
+        CHIME_ID = INDICATOR_ID_CHIME_GUITARRIFF
+        break;
+      case "windchimes":
+        CHIME_ID = INDICATOR_ID_CHIME_WINDCHIMES
+        break;
+      case "bingbong":
+        CHIME_ID = INDICATOR_ID_CHIME_BINGBONG
+        break;
+      case "ringdoorbell":
+        CHIME_ID = INDICATOR_ID_CHIME_RINGDOORBELL
+        break;
+    }
+    let VOLUME = volume * 20 - 1
+    let INDICATOR_CHIME = { id: CHIME_ID,property: PROPERTY_ID_VOLUME,value: VOLUME, }
+    this.setIndicator(INDICATOR_CHIME);
+  }
+  */
+
   async setIndicator(value) {
     this.log("Value received to send to indicator: ", value);
     this.node.sendCommand( this.IndicatorSet([value,]))
