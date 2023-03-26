a0543024bbb5c7b293bcbd5b791084f0e42725e3
Alistair Galbraith <alistair@alistairs.net>
2023-03-26 12:42:12 +0100

    Add tamper detection for v2 contact sensor

--- drivers/Contact Sensor v2/device.js
+++ drivers/Contact Sensor v2/device.js
@@ -11,15 +11,25 @@ class RingDevice extends ZwaveDevice {
     // register the measure_battery capability with COMMAND_CLASS_BATTERY
     this.registerCapability('measure_battery', 'BATTERY');
 
+    // Add tamper support if not present
+    if (!this.getCapabilities().includes("alarm_tamper")) {
+      this.addCapability('alarm_tamper');
+    }
+
     // register listener for NOTIFICATION REPORT
     this.registerReportListener('NOTIFICATION', 'NOTIFICATION_REPORT', report =>  {
+      this.log(`Notification ${JSON.stringify(report)}`);
       switch (report['Notification Type']) {
         case "Home Security":
           if ( report['Event'] == 2 ) {
             this.setCapabilityValue('alarm_contact', true)
+          } else if ( report['Event'] == 3 ) {
+            this.setCapabilityValue('alarm_tamper', true) 
           } else if ( report['Event'] == 0 ) {
             this.setCapabilityValue('alarm_contact', false)
+            this.setCapabilityValue('alarm_tamper', false)
           }
+          
           break;
       }
     });
