e8e083df873213811eaf49300243d84a6dcde379
Danee de Kruijff <daneedekruyff@outlook.com>
2023-06-26 20:32:30 +0200

    capabilities

--- drivers/Glass Break Sensor/device.js
+++ drivers/Glass Break Sensor/device.js
@@ -11,11 +11,28 @@ class MyDevice extends Device {
     this.log('Glass Break sensor has been initialized');
   }
 
+  async onNodeInit() {
+    // this.enableDebug();
+    // this.printNode();
+
+    // register the measure_battery capability with COMMAND_CLASS_BATTERY
+    this.registerCapability('measure_battery', 'BATTERY');   
+
+    // register listener for NOTIFICATION REPORT
+    this.registerReportListener('NOTIFICATION', 'NOTIFICATION_REPORT', report =>  {
+
+      this.log(report)
+      
+    });
+
+    this.log(`Ring Glass Break Sensor "${this.getName()}" capabilities have been initialized`);
+  }
+
   /**
    * onAdded is called when the user adds the device, called just after pairing.
    */
   async onAdded() {
-    this.log('Glass Break sensor  has been added');
+    this.log('Glass Break sensor has been added');
   }
 
   /**
@@ -27,7 +44,7 @@ class MyDevice extends Device {
    * @returns {Promise<string|void>} return a custom message that will be displayed
    */
   async onSettings({ oldSettings, newSettings, changedKeys }) {
-    this.log('Glass Break sensor  settings where changed');
+    this.log('Glass Break sensor settings where changed');
   }
 
   /**
@@ -36,14 +53,14 @@ class MyDevice extends Device {
    * @param {string} name The new name
    */
   async onRenamed(name) {
-    this.log('Glass Break sensor  was renamed');
+    this.log('Glass Break sensor was renamed');
   }
 
   /**
    * onDeleted is called when the user deleted the device.
    */
   async onDeleted() {
-    this.log('Glass Break sensor  has been deleted');
+    this.log('Glass Break sensor has been deleted');
   }
 
 }
