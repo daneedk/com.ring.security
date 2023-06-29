42fe0fdd838245e16dfc817249e4d748b5e6a6a6
Danee de Kruijff <daneedekruyff@outlook.com>
2023-06-29 21:17:06 +0200

    Glass Break Sensor

--- drivers/4AK1SZ-0EU0/device.js
+++ drivers/4AK1SZ-0EU0/device.js
@@ -69,27 +69,6 @@ class RingDevice extends ZwaveDevice {
     // Triggers
     this.sendPincodeTrigger = this.homey.flow.getDeviceTriggerCard('4AK1SZ-0EU0-sendPincode');
     this.sendEmergencyTrigger = this.homey.flow.getDeviceTriggerCard('4AK1SZ-0EU0-sendEmergencyKey');
-    // Conditions
-    // Actions
-    /* copied to driver.js
-    this.homey.flow.getActionCard('4AK1SZ-0EU0-activateSiren')
-      .registerRunListener( async ( args, state ) => {
-          this.activateSiren(args.sirenMode);
-          return Promise.resolve( true );
-      })
-    this.homey.flow.getActionCard('4AK1SZ-0EU0-deactivateSiren')
-      .registerRunListener( async ( args, state ) => {
-          this.deactivateSiren();
-          return Promise.resolve( true );
-      })  
-
-    
-    this.homey.flow.getActionCard('4AK1SZ-0EU0-soundChime')
-      .registerRunListener( async (args, state) => {
-        this.soundChime(args.chime,args.volume);
-        return Promise.resolve( true );
-      });   
-    */
 
     // register listener for Heimdall events
     this.homey.app.heimdallApp
--- drivers/Glass Break Sensor/device.js
+++ drivers/Glass Break Sensor/device.js
@@ -4,15 +4,6 @@ const { ZwaveDevice } = require('homey-zwavedriver');
 
 class RingDevice extends ZwaveDevice {
 
-  /**
-   * onInit is called when the device is initialized.
-   */
-  /*
-  async onInit() {
-    this.log('Glass Break sensor has been initialized');
-  }
-  */
-
   async onNodeInit() {
     this.enableDebug();
     this.printNode();
@@ -23,7 +14,7 @@ class RingDevice extends ZwaveDevice {
     // register listener for NOTIFICATION REPORT
     this.registerReportListener('NOTIFICATION', 'NOTIFICATION_REPORT', report =>  {
 
-      this.log(`Notification ${JSON.stringify(report)}`);
+      //this.log(`Notification ${JSON.stringify(report)}`);
       switch (report['Notification Type']) {
         case "Home Security":
           if ( report['Event'] == 2 ) {
@@ -31,16 +22,39 @@ class RingDevice extends ZwaveDevice {
           } else if ( report['Event'] == 3 ) {
             this.setCapabilityValue('alarm_tamper', true) 
           } else if ( report['Event'] == 0 ) {
+            this.log("Event Parameter:", report['Event Parameter']);
             this.setCapabilityValue('alarm_glassbreak', false)
             this.setCapabilityValue('alarm_tamper', false)
           }
-
           break;
+
+        case "System":
+          if ( report['Event'] == 5 ) {
+            this.enableTestLed();
+
+          }
+          break;
+
       }
 
     });
 
+    // register listener for Heimdall events
+    this.homey.app.heimdallApp
+      .on('realtime', (result,detail) => {
+          this.updateGlassBreakSensorFromHeimdall(result,detail);
+      })
+
     this.log(`Ring Glass Break Sensor "${this.getName()}" capabilities have been initialized`);
+  }
+
+  async enableTestLed( ) {
+    this.log('testrequest recieved');
+    // A controller application can send an Indicator command class with 
+    // the Indicator ID 0x50 (identify) to turn on the LED on the device.
+
+    //this.setIndicator(80); // Does this work? Does it turn of again?
+
   }
 
   // Flowcard actions
@@ -49,7 +63,6 @@ class RingDevice extends ZwaveDevice {
       this.log('enableDetection is true');
       
       await this.setSettings({
-        // set settingID 11 to 1
         11: 1,
       });
 
@@ -57,13 +70,28 @@ class RingDevice extends ZwaveDevice {
       this.log('enableDetection is false');
 
       await this.setSettings({
-        // set settingID 11 to 0
         11: 0,
       });
     }
 
   }
 
+  async updateGlassBreakSensorFromHeimdall(result,detail) {
+    this.log('Event from Heimdall, result:', result); // Surveillance Mode
+    this.log('Event from Heimdall, detail:', detail); // value: disarmed | armed | partially_armed
+
+  }
+
+  async setIndicator(value) {
+    this.log("Value received to send to indicator: ", value);
+    let buf = Buffer.from([value]);  
+    this.node.CommandClass.COMMAND_CLASS_INDICATOR.INDICATOR_SET({ Value: buf })
+      .then(this.log)
+      .catch(this.error);
+  }
+
+
+  // homeycompose added methods below
   /**
    * onAdded is called when the user adds the device, called just after pairing.
    */
@@ -84,7 +112,6 @@ class RingDevice extends ZwaveDevice {
     this.log('old:    ', oldSettings);
     this.log('new:    ', newSettings);
     this.log('change: ', changedKeys);
-
   }
 
   /**
